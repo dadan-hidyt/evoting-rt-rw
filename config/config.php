81279003c67e414d27d195fd9577deddc2fd22f4
dadan-hidyt <dadanhidyt@gmail.com>
2022-03-07 18:27:40 +0700

    menambahkan pengaturan database

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php
+//START code
+$config = array();
+#configurasi database
+$config['host']     = "localhost";
+$config['user']     = "root";
+$config['pass']     = "";
+$config['dbname']   = "db_pemilu_rt_rw";
+
+//END code
+?>

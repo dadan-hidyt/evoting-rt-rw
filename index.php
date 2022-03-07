218a28b5388fe30b522a02a94b8713bece09c949
dadan-hidyt <dadanhidyt@gmail.com>
2022-03-07 18:40:00 +0700

    fisrt

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @author dadan <dadanhidyt@gmail.com>
+ * Aplikasi pemilihan ketua rt/rw online berbasis web
+ */
+
+define("BASE_PATH",dirname(__FILE__));
+
+//including file configurasi
+include BASE_PATH."config/config.php";
+include BASE_PATH."inc/init.php";
+
+?>

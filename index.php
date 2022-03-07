fd5c2c66d23418c9bd4910f107e3b06587834175
dadan-hidyt <dadanhidyt@gmail.com>
2022-03-07 18:21:34 +0700

    Last commit

--- index.php
+++ index.php
@@ -4,6 +4,10 @@
  * Aplikasi pemilihan ketua rt/rw online berbasis web
  */
 
-//todo
+define("BASE_PATH",dirname(__FILE__));
+
+//including file configurasi
+include BASE_PATH."config/config.php";
+include BASE_PATH."inc/init.php";
 
 ?>
